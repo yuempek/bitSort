7bdd03e3fc56830b74ea4463e9ceb0bd07e5693c
yuempek <yuempek@yahoo.com>
2019-07-20 19:41:35 +0300

    Update bitsort.c

--- bitsort.c
+++ bitsort.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+     
 
-#define BUFFER_LENGTH 10000
-#define EXAMPLE_ARRAY_LENGHT 10
+#define BUFFER_LENGTH 1000000
+#define EXAMPLE_ARRAY_LENGHT 1000000
 #define INT_SIZE 32
 
 #define COUNT_INDEX 0
@@ -11,101 +13,118 @@
                 
 int exampleArray[EXAMPLE_ARRAY_LENGHT] = {0};
 
-void * freeIdx = 0;
-void * maxIdx = 0;
-void ** root;
+int ** freeIdx = 0;
+int ** maxIdx = 0;
+int ** root;
 
 int leafCount = 0;
 
-int ** sortedArrayValueIndexes;
+int *** sortedArrayValueIndexes;
 int indx = 0;
 
-void recurse(void ** currentNode, int digit) {
+void recurse(int ** currentNode, int digit) {
     if(digit == 0) { //this is leaf
-        sortedArrayValueIndexes[indx] = (int *)currentNode[VALUE_INDEX];
+        sortedArrayValueIndexes[indx] = currentNode;
         indx++;
         return;
     }
     
-    if (currentNode[0]) recurse(currentNode[0], digit-1);
-    if (currentNode[1]) recurse(currentNode[1], digit-1);  
+    if (currentNode[0]) recurse((int **) currentNode[0], digit-1);
+    if (currentNode[1]) recurse((int **) currentNode[1], digit-1);  
 }
 
-int ** getSortedValueAddresses(){
-    sortedArrayValueIndexes = (int **) malloc(leafCount * sizeof(int*));
+int *** getSortedValueAddresses(){
+    sortedArrayValueIndexes = (int ***) malloc(leafCount * sizeof(int**));
     // todo recursive binary tree search for all leaf
     recurse(root, INT_SIZE);
     return sortedArrayValueIndexes;
 }
             
 
-void * nextFreeIndex(){
-    if((freeIdx + 2*sizeof(void *)) > maxIdx) {
+int ** nextFreeIndex(){
+    if((freeIdx + 2) >= maxIdx) {
         
-        void ** newBuffer = (void **) malloc(BUFFER_LENGTH * 2 * sizeof(void*));
-        
-        for (int i = 0; i < BUFFER_LENGTH; i++) 
-            newBuffer[BUFFER_LENGTH] = 0;
+        int ** newBuffer = (int **) malloc(BUFFER_LENGTH * sizeof(int*));
+        int i;
+        for (i = 0; i < BUFFER_LENGTH; i++) 
+            newBuffer[i] = 0;
         
         freeIdx = newBuffer;
         maxIdx = newBuffer + BUFFER_LENGTH;
     } else {
-        freeIdx += 2*sizeof(void *);
+        freeIdx = freeIdx + 2;
     }
     
     return freeIdx;
 } 
 
 void bitSort(int * array, int arraySize) {
-    root = nextFreeIndex();
+	int i, j;
+	int ** addr;
 
-    for (int i = 0; i < arraySize; i++) {
-        void ** activeNode = root;
+	root = (int**) nextFreeIndex();
+
+    for (i = 0; i < arraySize; i++) {
+        int ** activeNode = root;
         int value = array[i];
     
-        int digit = 1 << (INT_SIZE-1);
+        unsigned int digit = 1 << (INT_SIZE-1);
         int bit;
 
-        for (int j = INT_SIZE; j > 0 && digit != 0; j--){
+        for (j = INT_SIZE; j > 0 && digit != 0; j--){
             bit = (digit & value) ? 1 : 0;
             
-            void * addr = activeNode[bit];
+            addr = (int **) activeNode[bit];
             
             if (addr == 0) {
                 addr = nextFreeIndex();
-                activeNode[bit] = addr;
+                activeNode[bit] = (int *) addr;
             }
             
-            activeNode = addr;
+            activeNode = (int **) addr;
             digit = digit >> 1;
         }
         
-        if (activeNode == 0) {
-            activeNode[COUNT_INDEX] = (void *) 1;
+        if (*activeNode == 0) {
+            activeNode[COUNT_INDEX] = 1;
             activeNode[VALUE_INDEX] = array + i;
             leafCount++;
         }
         else{
-            activeNode[COUNT_INDEX]++;
+            activeNode[COUNT_INDEX] = ((int)activeNode[COUNT_INDEX]) + 1;
         }      
     }
 }
 
 int main() {
-   for (int i = 0; i < EXAMPLE_ARRAY_LENGHT; i++) exampleArray[i] = i;
-   
-   printf("Begin \n");
-   
-   bitSort(exampleArray, EXAMPLE_ARRAY_LENGHT);
-   
-   int ** valueadresses = getSortedValueAddresses();
+    int *** leafAdresses;
+	int i, count;
 
-   printf("leafCount : %d \n", leafCount);
+	clock_t start, end;
+    double cpu_time_used;
+     
+    
+
+
+    for (i = 0; i < EXAMPLE_ARRAY_LENGHT; i++) exampleArray[i] = i;
    
-   for(int i = 0; i < leafCount; i++)
-      printf("%d-", *(valueadresses[i]));
+    printf("Begin \n");
    
-   printf("End");
+    start = clock();
+	    bitSort(exampleArray, EXAMPLE_ARRAY_LENGHT);
+		leafAdresses = getSortedValueAddresses();
+	end = clock();
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+	printf("leafCount : %d\n", leafCount);
+	printf("duration  : %d ms\n", (int)(cpu_time_used * 1000));
+   
+   /* for(i = 0; i < leafCount; i++){
+		count = (int) leafAdresses[i][COUNT_INDEX];
+		while(count--)
+			printf("%d-", *(leafAdresses[i][VALUE_INDEX]));
+	}*/
+    printf("End");
+	system("pause");
 }
-
-
