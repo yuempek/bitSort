167b1227b7f79ef64269377586a8d04076e8f57c
yuempek <yuempek@yahoo.com>
2019-07-22 12:50:31 +0300

    Update bitsort.c

--- bitsort.c
+++ bitsort.c
@@ -21,6 +21,8 @@ int ** root;
 int leafCount = 0;
 int nodeCount = 0;
 
+double duration_nextFreeIndex = 0;
+	
 
 int *** sortedArrayValueIndexes;
 int indx = 0;
@@ -42,20 +44,16 @@ int *** getSortedValueAddresses(){
     recurse(root, INT_SIZE);
     return sortedArrayValueIndexes;
 }
-            
 
 int ** nextFreeIndex(){
-    if((freeIdx + 2) >= maxIdx) {
-        
+    if((freeIdx + 2) < maxIdx) {
+        freeIdx = freeIdx + 2;
+    } else {
         int ** newBuffer = (int **) malloc(BUFFER_LENGTH * sizeof(int*));
         int i;
-        //for (i = 0; i < BUFFER_LENGTH; i++) 
-        //    newBuffer[i] = 0;
-        
+
         freeIdx = newBuffer;
         maxIdx = newBuffer + BUFFER_LENGTH;
-    } else {
-        freeIdx = freeIdx + 2;
     }
     
     freeIdx[0] = 0;
@@ -65,22 +63,44 @@ int ** nextFreeIndex(){
 
     return freeIdx;
 } 
+/*
+Bit sort is a new sorting algorithm that uses binary tree to sort elements. Numbers are put in a binary tree. When reading they sorted already. Complexity is O(nk). k is bit size of the number. BitSort is not recursive algorithm when sort the values. But reading the tree is recursive.
 
+the full size of binary tree:
+                        ______________________x______________________                          
+                      0/                                             \1                        
+            __________x__________                           __________x__________              
+          0/                     \1                       0/                     \1            
+      ____x____               ____x____               ____x____               ____x____        
+    0/         \1           0/         \1           0/         \1           0/         \1      
+   _x_         _x_         _x_         _x_         _x_         _x_         _x_         _x_     
+ 0/   \1     0/   \1     0/   \1     0/   \1     0/   \1     0/   \1     0/   \1     0/   \1   
+cnt  value  cnt  value  cnt  value  cnt  value  cnt  value  cnt  value  cnt  value  cnt  value 
+  
+Example:
+if 
+array = {};
+
+*/
 void bitSort(int * array, int arraySize) {
 	int i, j;
 	int ** addr;
-
+    clock_t start, end;
+  
 	root = (int**) nextFreeIndex();
 
     for (i = 0; i < arraySize; i++) {
+            
+        
         int ** activeNode = root;
-        int value = array[i];
     
-        unsigned int digit = 1 << (INT_SIZE-1);
-        int bit;
+        register int value = array[i];
+        register int bit;
 
-        for (j = INT_SIZE; j > 0 && digit != 0; j--){
-            bit = (digit & value) ? 1 : 0;
+        for (j = INT_SIZE; j; j--){
+                
+            bit = (0x80000000 & value) >> 31;
+            value <<= 1;
             
             addr = (int **) activeNode[bit];
             
@@ -88,11 +108,9 @@ void bitSort(int * array, int arraySize) {
                 addr = nextFreeIndex();
                 activeNode[bit] = (int *) addr;
             }
-            
+
             activeNode = (int **) addr;
-            digit = digit >> 1;
         }
-        
         if (*activeNode == 0) {
             activeNode[COUNT_INDEX] = 1;
             activeNode[VALUE_INDEX] = array + i;
@@ -100,39 +118,45 @@ void bitSort(int * array, int arraySize) {
         }
         else{
             activeNode[COUNT_INDEX] = ((int)activeNode[COUNT_INDEX]) + 1;
-        }      
+        }     
+             
     }
 }
 
+
+//start = clock();
+//duration_nextFreeIndex += ((double) (clock() - start)) / CLOCKS_PER_SEC * 1000000;
+
 int main() {
-  int *** leafAdresses;
+    int *** leafAdresses;
 	int i, count;
-  int duration1, duration2;
+    int duration1, duration2;
 	clock_t start, end;
-  double cpu_time_used;
+    double cpu_time_used;
      
 
-  for (i = 0; i < EXAMPLE_ARRAY_LENGHT; i++) 
-      exampleArray[i] = randomd() * 0x7FFFFFFF;
+    for (i = 0; i < EXAMPLE_ARRAY_LENGHT; i++) 
+        exampleArray[i] = randomd() * 0x7FFFFFFF;
    
-  printf("Begin \n");
+    printf("Begin \n");
    
-  start = clock();
-	  bitSort(exampleArray, EXAMPLE_ARRAY_LENGHT);
+    start = clock();
+	    bitSort(exampleArray, EXAMPLE_ARRAY_LENGHT);
 	end = clock();
-  duration1 = ((double) (end - start)) / CLOCKS_PER_SEC * 1000;
+    duration1 = ((double) (end - start)) / CLOCKS_PER_SEC * 1000000;
 
-  start = clock();
+    start = clock();
 		leafAdresses = getSortedValueAddresses();
 	end = clock();
-  duration2 = ((double) (end - start)) / CLOCKS_PER_SEC * 1000;
+    duration2 = ((double) (end - start)) / CLOCKS_PER_SEC * 1000000;
 
-  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 
 	printf("leafCount : %d\n", leafCount);
 	printf("nodeCount : %d\n", nodeCount);
-	printf("duration sort : %d ms\n", duration1);
-  printf("duration read : %d ms\n", duration2);
+	printf("duration sort : %d us\n", duration1);
+    printf("duration read : %d us\n", duration2);
+    printf("duration nextFreeIndex : %d us\n", (int)duration_nextFreeIndex);
    
   /*
   for(i = 0; i < leafCount; i++){
