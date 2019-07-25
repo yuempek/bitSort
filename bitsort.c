8d0c7fbb210f68c3c441672c97c416d06e5892df
yuempek <yuempek@yahoo.com>
2019-07-25 11:30:16 +0300

    Update bitsort.c

--- bitsort.c
+++ bitsort.c
@@ -71,7 +71,7 @@ void recurse(Block* currentNode, char digit) {
 Block** getSortedValueBlockAddresses(){
     if(sortedValueBlockAddresses) free(sortedValueBlockAddresses);
     sortedValueBlockAddresses = malloc(leafCount * sizeof(Block*));
-    recurse(root, INT_SIZE);
+    recurse(root, INT_SIZE - 1);
     return sortedValueBlockAddresses;
 }
 
@@ -100,16 +100,15 @@ Block* nextFreeBlock(){
 # BitSort
 Bit sort is a new sorting algorithm that uses binary tree to sort elements. Numbers are put in a binary tree. When reading they sorted already. Complexity is **O(nk)**. **k** is bit size of the number. BitSort is not recursive algorithm when sorting the values. But reading the tree is recursive.
 the full size of binary tree for 3bit values:
-                            ______________________|______________________                          
-                           0                                             1                         
-                __________/__________                           __________\__________              
-               0                     1                         0                     1             
-          ____/____               ____\____               ____/____               ____\____        
-         0         1             0         1             0         1             0         1       
-       _/_         _\_         _/_         _\_         _/_         _\_         _/_         _\_     
-      /   \       /   \       /   \       /   \       /   \       /   \       /   \       /   \    
-    cnt  value  cnt  value  cnt  value  cnt  value  cnt  value  cnt  value  cnt  value  cnt  value 
-  
+
+                            0_____________________|_____________________1                          
+                           /                                             \                         
+                0_________/_________1                           0_________\_________1              
+               /                     \                         /                     \             
+          0___/___1               0___\___1               0___/___1               0___\___1        
+         /         \             /         \             /         \             /         \       
+       cnt         cnt         cnt         cnt         cnt         cnt         cnt         cnt     
+   
 Example:
 We suppose that we have 3 bit length numbers.
     array = {7, 3, 2, 5, 0, 7, 3, 2, 7};
@@ -120,52 +119,51 @@ We suppose that we have 3 bit length numbers.
     
                   msb       lsb
                    L1   L2   L3
-    7 = 111  -->    1    1    1
-    3 = 011  -->    0    1    1
-    2 = 010  -->    0    1    0
-    5 = 101  -->    1    0    1
-    0 = 000  -->    0    0    0
-    7 = 111  -->    1    1    1
-    3 = 011  -->    0    1    1
-    2 = 010  -->    0    1    0
-    7 = 111  -->    1    1    1
+    7 = 111  -->    \    \    1
+    3 = 011  -->    /    \    1
+    2 = 010  -->    /    \    0
+    5 = 101  -->    \    /    1
+    / = 000  -->    /    /    0
+    7 = 111  -->    \    \    1
+    3 = 011  -->    /    \    1
+    2 = 010  -->    /    \    0
+    7 = 111  -->    \    \    1
 firstly binary tree has only root node.
     
-                            ______________________|______________________                          
-                           0                                             1                         
+                            0_____________________|_____________________1                          
+                           /                                             \                         
+
 first number is added to binary tree using own bits from msb to lsb. 
 (adding number: 7)
-                                ______________________|______________________                          
-    L1  -------------------->  0                                             1                         
-                                                                    __________\__________              
-    L2  -------------------------------------------------------->  0                     1             
-                                                                                      ____\____        
-    L3  ------------------------------------------------------------------------->   0         1       
-                                                                                               _\_     
-                                                                                              /   \    
-                                                                                             1     7 
+
+                                0_____________________|_____________________1                          
+    L1  -------------------->                                                \                         
+                                                                    0_________\_________1              
+    L2  -------------------------------------------------------->                        \             
+                                                                                      0___\___1        
+    L3  ------------------------------------------------------------------------->             \       
+                                                                                               [1]     
+
 Then others sequently.
 (adding numbers: 3, 2, 5, 0)
-                            ______________________|______________________                          
-                           0                                             1                         
-                __________/__________                           __________\__________              
-               0                     1                         0                     1             
-          ____/____               ____\____               ____/____               ____\____        
-         0         1             0         1             0         1             0         1       
-       _/_                     _/_         _\_                     _\_                     _\_     
-      /   \                   /   \       /   \                   /   \                   /   \    
-    [1     0]               [1     2]   [1     3]               [1     5]                1     7 
+                            0_____________________|_____________________1                          
+                           /                                             \                         
+                0_________/_________1                           0_________\_________1              
+               /                     \                         /                     \             
+          0___/___1               0___\___1               0___/___1               0___\___1        
+         /                       /         \                       \                       \       
+       [1]                     [1]         [1]                     [1]                      1     
+
 if a number is already in tree, its count is increased 1.
 (numbers: 7, 3, 2, 7)
-                            ______________________|______________________                          
-                           0                                             1                        
-                __________/__________                           __________\__________              
-               0                     1                         0                     1            
-          ____/____               ____\____               ____/____               ____\____        
-         0         1             0         1             0         1             0         1       
-       _/_                     _/_         _\_                     _\_                     _\_     
-      /   \                   /   \       /   \                   /   \                   /   \    
-     1     0                [2]    2    [2]    3                 1     5                [3]    7 
+                            0_____________________|_____________________1                          
+                           /                                             \                        
+                0_________/_________1                           0_________\_________1              
+               /                     \                         /                     \            
+          0___/___1               0___\___1               0___/___1               0___\___1        
+         /                       /         \             /         \             /         \       
+       [1]                     [2]         [2]                     [1]                     [3]     
+
 When it is read recursively, can be get sorted array.
     sorted_array =  [1x0, 2x2, 2x3, 1x5, 3x7]
     sorted_array = [0, 2, 2, 3, 3, 5, 7, 7, 7]
@@ -190,7 +188,7 @@ void bitSort(int * array, int arraySize) {
         //printf("\n");
 
         //for every bit of value 
-        for (j = 0; j < INT_SIZE; j++){
+        for (j = 0; j < INT_SIZE - 1; j++){
             //from msb to lsb    
             bit = (digit & (value << j)) >> (INT_SIZE - 1);
 
@@ -244,7 +242,7 @@ int main() {
     for (i = 0; i < EXAMPLE_ARRAY_LENGHT; i++) {
         exampleArray[i] = randomd() * 50;//0x7FFFFFFF;
 #ifdef OUTPUT
-       printf("%d ", exampleArray[i]);
+       printf("%d\n", exampleArray[i]);
 #endif
     }
 
@@ -281,7 +279,7 @@ int main() {
     for(i = 0; i < leafCount; i++){
 		count = leafAdresses[i]->leaf.cnt;
 		while(count--)
-			printf("%d ", leafAdresses[i]->leaf.value);
+			printf("%d\n", leafAdresses[i]->leaf.value);
     }
 #endif
   
