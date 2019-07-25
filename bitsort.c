f0bc9ffd273644ae3764e7aaade3e8139ce2873d
yuempek <yuempek@yahoo.com>
2019-07-25 11:15:21 +0300

    used "union & struct" for blocks

--- bitsort.c
+++ bitsort.c
@@ -1,61 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "randomd.h"
-     
 
-#define BUFFER_LENGTH 1000000
-#define EXAMPLE_ARRAY_LENGHT 1000
+#include <time.h>
+#include <stdlib.h>
+
+
+void sleep(double t){
+    char time[100];
+    sprintf(time, "sleep %f", t);
+    fflush(stdout);            
+    system(time);
+}
+
+double randomd()   /* uniform distribution [0..1] */
+{
+  int i;
+  unsigned long x = 0;
+
+  srand(clock());
+  for(i = 0; i < 8; i++)
+      x = (x << 8) | (rand() & 0xFF);
+
+  return 1.0*x / (unsigned long)(-1);
+}
+
+
+typedef union Block Block;
+
+typedef union Block {
+    struct {
+        int cnt;
+        int value;
+    } leaf;
+
+    Block* node[2];
+} Block;
+
+#define OUTPUT
+#define EXAMPLE_ARRAY_LENGHT 100
+#define BUFFER_LENGTH EXAMPLE_ARRAY_LENGHT
 #define INT_SIZE 32
-
-#define COUNT_INDEX 0
-#define VALUE_INDEX 1
 
                 
 int exampleArray[EXAMPLE_ARRAY_LENGHT] = {0};
 
-int ** freeIdx = 0;
-int ** maxIdx = 0;
-int ** root;
+int freeIdxCnt = 0;
+
+Block* freeIdx = 0;
+Block* root;
 
 int leafCount = 0;
 int nodeCount = 0;
+int mem = 0;
 
-
-int *** sortedArrayValueIndexes;
+Block** sortedValueBlockAddresses;
 int indx = 0;
 
-void recurse(int ** currentNode, int digit) {
+void recurse(Block* currentNode, char digit) {
     if(digit == 0) { //this is leaf
-        sortedArrayValueIndexes[indx] = currentNode;
+        sortedValueBlockAddresses[indx] = currentNode;
         indx++;
         return;
     }
     
-    if (currentNode[0]) recurse((int **) currentNode[0], digit-1);
-    if (currentNode[1]) recurse((int **) currentNode[1], digit-1);  
+    if (currentNode->node[0]) recurse(currentNode->node[0], digit-1);
+    if (currentNode->node[1]) recurse(currentNode->node[1], digit-1);  
 }
 
-int *** getSortedValueAddresses(){
-    sortedArrayValueIndexes = (int ***) malloc(leafCount * sizeof(int**));
-    // todo recursive binary tree search for all leaf
+Block** getSortedValueBlockAddresses(){
+    if(sortedValueBlockAddresses) free(sortedValueBlockAddresses);
+    sortedValueBlockAddresses = malloc(leafCount * sizeof(Block*));
     recurse(root, INT_SIZE);
-    return sortedArrayValueIndexes;
+    return sortedValueBlockAddresses;
 }
 
-int ** nextFreeIndex(){
-    if((freeIdx + 2) < maxIdx) {
-        freeIdx = freeIdx + 2;
-    } else {
-        int ** newBuffer = (int **) malloc(BUFFER_LENGTH * sizeof(int*));
-        int i;
+Block* initBlockBuffer(){
+    freeIdx = malloc((BUFFER_LENGTH) * sizeof(Block));
+    freeIdxCnt = BUFFER_LENGTH - 1;
+    mem += BUFFER_LENGTH * sizeof(Block);
+    return freeIdx;
+}
 
-        freeIdx = newBuffer;
-        maxIdx = newBuffer + BUFFER_LENGTH;
-    }
+Block* nextFreeBlock(){
     
-    freeIdx[0] = 0;
-    freeIdx[1] = 0;
+    if(freeIdxCnt--) 
+        freeIdx++;
+    else 
+        initBlockBuffer();
+
+    freeIdx->node[0] = 0;
+    freeIdx->node[1] = 0;
     
     nodeCount++;
 
@@ -63,11 +98,8 @@ int ** nextFreeIndex(){
 } 
 /*
 # BitSort
-
 Bit sort is a new sorting algorithm that uses binary tree to sort elements. Numbers are put in a binary tree. When reading they sorted already. Complexity is **O(nk)**. **k** is bit size of the number. BitSort is not recursive algorithm when sorting the values. But reading the tree is recursive.
-
 the full size of binary tree for 3bit values:
-
                             ______________________|______________________                          
                            0                                             1                         
                 __________/__________                           __________\__________              
@@ -80,7 +112,6 @@ the full size of binary tree for 3bit values:
   
 Example:
 We suppose that we have 3 bit length numbers.
-
     array = {7, 3, 2, 5, 0, 7, 3, 2, 7};
     
     L   : level
@@ -98,17 +129,12 @@ We suppose that we have 3 bit length numbers.
     3 = 011  -->    0    1    1
     2 = 010  -->    0    1    0
     7 = 111  -->    1    1    1
-
 firstly binary tree has only root node.
     
                             ______________________|______________________                          
                            0                                             1                         
-
-
 first number is added to binary tree using own bits from msb to lsb. 
 (adding number: 7)
-
-
                                 ______________________|______________________                          
     L1  -------------------->  0                                             1                         
                                                                     __________\__________              
@@ -118,11 +144,8 @@ first number is added to binary tree using own bits from msb to lsb.
                                                                                                _\_     
                                                                                               /   \    
                                                                                              1     7 
-
 Then others sequently.
 (adding numbers: 3, 2, 5, 0)
-
-
                             ______________________|______________________                          
                            0                                             1                         
                 __________/__________                           __________\__________              
@@ -132,11 +155,8 @@ Then others sequently.
        _/_                     _/_         _\_                     _\_                     _\_     
       /   \                   /   \       /   \                   /   \                   /   \    
     [1     0]               [1     2]   [1     3]               [1     5]                1     7 
-
-
 if a number is already in tree, its count is increased 1.
 (numbers: 7, 3, 2, 7)
-
                             ______________________|______________________                          
                            0                                             1                        
                 __________/__________                           __________\__________              
@@ -146,64 +166,63 @@ if a number is already in tree, its count is increased 1.
        _/_                     _/_         _\_                     _\_                     _\_     
       /   \                   /   \       /   \                   /   \                   /   \    
      1     0                [2]    2    [2]    3                 1     5                [3]    7 
-
 When it is read recursively, can be get sorted array.
-
     sorted_array =  [1x0, 2x2, 2x3, 1x5, 3x7]
     sorted_array = [0, 2, 2, 3, 3, 5, 7, 7, 7]
-
 */
 void bitSort(int * array, int arraySize) {
 	int i, j;
-	int ** addr;
+	Block* block;
     clock_t start, end;
 
     //create a buffer. root node is first node in buffer. 
-	root = (int**) nextFreeIndex();
-
+	root = initBlockBuffer();
+    const unsigned long long digit = ((unsigned long long) 1) << (INT_SIZE - 1);
     //for every array element
     for (i = 0; i < arraySize; i++) {
             
         // start at root
-        int ** activeNode = root;
+        Block* activeBlock = root;
     
         register int value = array[i];
         register int bit;
 
+        //printf("\n");
+
         //for every bit of value 
-        for (j = INT_SIZE; j; j--){
+        for (j = 0; j < INT_SIZE; j++){
             //from msb to lsb    
-            bit = (0x80000000 & value) >> 31;
-            value <<= 1;
-            
+            bit = (digit & (value << j)) >> (INT_SIZE - 1);
+
             // get the related node from bit 
-            addr = (int **) activeNode[bit];
+            block = activeBlock->node[bit];
+            
             
             // if the node is not exists
-            if (addr == 0) {
+            if (block == 0) {
                 // get next blank node from the buffer.
-                addr = nextFreeIndex();
+                block = nextFreeBlock();
 
                 //connect new node to previous node  
-                activeNode[bit] = (int *) addr;
+                activeBlock->node[bit] = block;
             }
 
             // jump to new node.
-            activeNode = (int **) addr;
+            activeBlock = block;
         }
 
         // moved on every bits in tree 
-        if (*activeNode == 0) { 
+        if (activeBlock->leaf.cnt == 0) { 
             // but there is no value specified address(if cnt index is 0(zero))
             // set the value.
-            activeNode[COUNT_INDEX] = 1;
-            activeNode[VALUE_INDEX] = array + i;
+            activeBlock->leaf.cnt = 1;
+            activeBlock->leaf.value = array[i];
             
             leafCount++; // count the leafs
         }
         else{
             // then if value set before, increase the count. 
-            activeNode[COUNT_INDEX] = ((int)activeNode[COUNT_INDEX]) + 1;
+            activeBlock->leaf.cnt++;
         }     
              
     }
@@ -211,7 +230,7 @@ void bitSort(int * array, int arraySize) {
 
 
 int main() {
-    int *** leafAdresses;
+    Block** leafAdresses;
 	int i, count;
     int duration1, duration2;
 	clock_t start, end;
@@ -223,9 +242,12 @@ int main() {
     printf("\n");
     printf("unsorted array(%d) : ", EXAMPLE_ARRAY_LENGHT); 
     for (i = 0; i < EXAMPLE_ARRAY_LENGHT; i++) {
-        exampleArray[i] = randomd() * 50; //* 0x7FFFFFFF;
-        printf("%d ", exampleArray[i]);
+        exampleArray[i] = randomd() * 50;//0x7FFFFFFF;
+#ifdef OUTPUT
+       printf("%d ", exampleArray[i]);
+#endif
     }
+
 
     printf("\n");
     printf("\n");
@@ -234,17 +256,19 @@ int main() {
     start = clock();
         //sorting 
         bitSort(exampleArray, EXAMPLE_ARRAY_LENGHT);
-	end = clock();
+	  end = clock();
     duration1 = ((double) (end - start)) / CLOCKS_PER_SEC * 1000000;
 
     start = clock();
 		//reading fom tree 
-        leafAdresses = getSortedValueAddresses();
+        leafAdresses = getSortedValueBlockAddresses();
 	end = clock();
     duration2 = ((double) (end - start)) / CLOCKS_PER_SEC * 1000000;
 
 	printf("leafCount : %d\n", leafCount);
 	printf("nodeCount : %d\n", nodeCount);
+	printf("memory : %d\n", mem);
+	printf("sizeof(Block) : %ld\n", sizeof(Block));
 	printf("duration sort : %d us\n", duration1);
     printf("duration read : %d us\n", duration2);
    
@@ -252,14 +276,18 @@ int main() {
     printf("\n");
     printf("\n");
     printf("sorted array(%d) : ", EXAMPLE_ARRAY_LENGHT); 
+
+#ifdef OUTPUT
     for(i = 0; i < leafCount; i++){
-		count = (int) leafAdresses[i][COUNT_INDEX];
+		count = leafAdresses[i]->leaf.cnt;
 		while(count--)
-			printf("%d ", *(leafAdresses[i][VALUE_INDEX]));
+			printf("%d ", leafAdresses[i]->leaf.value);
     }
+#endif
   
   
     printf("\n");
     printf("\n");
     printf("Done");
 }
+
